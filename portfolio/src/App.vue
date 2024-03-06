5c990c0d18fec0c25791bc1958c3b624bfb45152
7ntys <61086353+7ntys@users.noreply.github.com>
2024-03-06 12:07:39 +0100

    New website

---
+++ portfolio/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HomePage />
+  </div>
+</template>
+
+<script>
+
+import HomePage from "./components/HomePage.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 auto;
+  background-color: #011126;
+}
+body{
+  background: #011126;
+}
+</style>
